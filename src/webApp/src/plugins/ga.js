5942473ee48a1172b1443df1c2ff60c17de0ad98
jules goullée <julesgoullee@gmail.com>
2018-05-27 06:14:52 +0200

    ga config

--- src/webApp/src/plugins/ga.js
+++ src/webApp/src/plugins/ga.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
 import config from '@/../../config';
+import router from '@/router';
 
 if(config.GA_ID){
 
   Vue.use(VueAnalytics, {
-    id: config.GA_ID
+    id: config.GA_ID,
+    router,
+    debug: {
+      screenview: true,
+      skipSamePath: true,
+      enabled: config.ENV !== 'production',
+      sendHitTask: config.ENV === 'production'
+    }
   });
 
 }
